3b9ef0a07dcb9f985d114ef9ec986994925930df
rizkyindra <rizkyindramaulana@student.uns.ac.id>
2023-03-06 10:03:08 +0700

    commit pertama

---
+++ Modul_01/indra.py
@@ -0,0 +1,13 @@
+nama depan = "Rizky"
+nama blk = "indra"
+
+angka = 10
+desimal = 0,5
+boolean = True
+kelas_D = ["rizky", "kls d", "dll"]
+data = {"nama": "rizky", "kelas": "KLS D"}
+
+print(nama_depan)
+print("nama saya adalah rizky")
+
+print(f"nama saya adalah {nama_depan} {nama_blk} dari kelas {data['kelas']}")
